7575814064b506b1448230cac3e4cb5bd171f997
FloCode2010 <108221218+FloCode2010@users.noreply.github.com>
2025-11-15 19:46:08 +0100

    Fixed the Merge

--- main.py
+++ main.py
@@ -23,8 +23,82 @@ seaLevel = None             # Meeresspiegel (m)                                 
 # winterLength = None         # Winterlänge (Tage)                                    Link:              
 # permafrostDepth = None      # Permafrost-Tiefe (m)                                  Link:     
 # energyMix = None            # Energie-Mix (% Erneuerbare Energien)                  Link:
+deaths = None                 # Todesfälle durch Klimakatastrophen
+with open('events.JSON', 'r') as file:
+    data = json.loads(file)
 
+## Classes
 
+class climateEvent:
+    name=None
+    # indexNumber=None
+    eventText=None
+    changingParameter1=None
+    changingParameter2=None
+    changingParameter3=None
+    weighting1=None
+    weighting2=None
+    weighting3=None
+    propability=None
+    change1=None
+    change2=None
+    change3=None
+    change4=None
+
+    def effects(change1, change2, change3, change4, eventText):
+        globalTemp += change1
+        ggc += change2
+        seaLevel += change3
+        deaths += change4 ## höherer Faktor bei stärkeren Event
+        print(eventText)
+
+    def normalise(min, max, value):
+        return (value - min) / (max - min)
+
+    def calculatePropability(weighting1, weighting2, weighting3):
+        return (weighting1 * climateEvent.normalise(0, 5, globalTemp) + weighting2 * climateEvent.normalise(250, 40000, ggc) + weighting3 * seaLevel / (weighting1 + weighting2 + weighting3))
+
+    def propabilityCheck(propability, effects=effects, change1=change1, change2=change2, change3=change3, eventText=eventText):
+        import random
+        randNum = random.random()
+        if(randNum <= propability):
+            effects(change1, change2, change3, eventText)
+            return True
+        else:
+            return False
+        
+acidRain = climateEvent()
+icebergMelt = climateEvent()
+bushFire = climateEvent()
+flooding = climateEvent()
+drought = climateEvent()
+permFrostBurst = climateEvent()
+listOfEvents = [acidRain, icebergMelt, bushFire, flooding, drought, permFrostBurst]  # Durch diese Liste wird später durchiteriert um die Ereignisse zu definieren
+
+class Simulation:
+    def initialise():
+        for i in range(len(listOfEvents)):
+            listOfEvents[i].name = data[str(i)]["name"]
+            listOfEvents[i].eventText = data[str(i)]["descr"]
+            listOfEvents[i].change1 = data[str(i)]["change"]["globalTemp"]
+            listOfEvents[i].change2 = data[str(i)]["change"]["seaLevel"]
+            listOfEvents[i].change3 = data[str(i)]["change"]["ggc"]
+            listOfEvents[i].change4 = data[str(i)]["change"]["deaths"]
+            listOfEvents[i].weighting1 = data[str(i)]["parameterWeighting"]["globalTemp"]
+            listOfEvents[i].weighting2 = data[str(i)]["parameterWeighting"]["ggc"]
+            listOfEvents[i].weighting3 = data[str(i)]["parameterWeighting"]["seaLevel"]
+            # add weighting parameters
+            listOfEvents[i].calculatePropability()
+        acidRain = listOfEvents[0]
+        icebergMelt = listOfEvents[1]
+        bushFire = listOfEvents[2]
+        flooding = listOfEvents[3]
+        drought = listOfEvents[4]
+        permFrostBurst = listOfEvents[5]   
+    def checkPropabilities():
+        for i in range(len(listOfEvents)):
+            listOfEvents[i].propabilityCheck(listOfEvents[i].propability)    
+            
 ## Main Program
 
 print("CLIMATE++\n \n \n Wählen Sie eine Option aus indem sie die zugehörige Nummer eingeben: \n 1. Simulation starten\n 2. Programm verlassen")
@@ -39,6 +113,7 @@ if(user_input == "1"):
     print("Geben sie den Anfangs-Meeresspiegel (in m ueber Normal Null): ")
     seaLevel = float(input())
     print("\n\nSimulation hat mit den folgenden Parameterm gestartet: \n Treibhausgaskonzentration: " + str(ggc) + " ppm CO2-Äquivalent \n globale Temperatur: " + str(globalTemp) + " °C \n Wasserpegel: " + str(seaLevel) + " m ueber Normal Null")
+    Simulation.initialise()
     print("\n\n--- SIMULATION LAEUFT ---\n\n")
     print("Um das Programm zu verlassen, drücken sie Escape. Einige der folgenden Ereignisse werden von ihnen verlangen mit dem Programm zu interagieren.")
     while not keyboard.is_pressed('esc'):
